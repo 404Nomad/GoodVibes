313afab2238f55a586df6c964ac5341bb2135b89
404Nomad <richtofen60@gmail.com>
2025-02-28 15:08:39 +0100

    ajout function pour calculer le layout par rapport a la taille d'acran  ( dynamique )

--- app/src/main/java/com/cfa/goodvibes/MainActivity.kt
+++ app/src/main/java/com/cfa/goodvibes/MainActivity.kt
@@ -5,16 +5,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.tooling.preview.Preview
 import com.cfa.goodvibes.composant.AffirmationCard
+import com.cfa.goodvibes.composant.AffirmationList
 import com.cfa.goodvibes.ui.theme.GoodVibesTheme
 import com.cfa.goodvibes.composant.TextScreen
+import com.cfa.goodvibes.data.Datasource
 import com.cfa.goodvibes.model.Affirmation
 
 
@@ -35,10 +45,31 @@ fun HomeApp() {
     Scaffold(
         modifier = Modifier.fillMaxSize()
     ) { innerPadding ->
-        Column(Modifier.padding(innerPadding)) {
-            TextScreen()
-            AutreText()
-        }
+        AffirmationApp(Modifier.padding(innerPadding))
+    }
+}
+
+// calculer layout a gauche et a droite pour avoir la maxPadding par rapport a l'écran cible
+@Composable
+fun AffirmationApp(
+    modifier: Modifier = Modifier
+){
+    val layoutDirection = LocalLayoutDirection.current
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+            .padding(
+                start = WindowInsets.safeDrawing
+                    .asPaddingValues()
+                    .calculateStartPadding(layoutDirection),
+                end = WindowInsets.safeDrawing
+                    .asPaddingValues()
+                    .calculateEndPadding(layoutDirection)
+            )
+    ) {
+        AffirmationList(
+            affirmationList = Datasource().loadAffirmation())
     }
 }
 
@@ -59,9 +90,9 @@ fun TextScreenPreview() {
 @Composable
 fun CardPreview() {
     GoodVibesTheme {
-        AffirmationCard(
-            affirmation =
-            Affirmation(R.string.affirmation1, R.drawable.image1)
+        AffirmationList(
+            affirmationList = Datasource().loadAffirmation()
+
         )
     }
 }
